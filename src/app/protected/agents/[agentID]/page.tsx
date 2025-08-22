768675496aca0cf6c614c33dee6b5761ccfe8125
tcelue <cs@tcelue.com>
2025-08-22 15:36:12 +0800

    &&&

--- src/app/protected/agents/[agentID]/page.tsx
+++ src/app/protected/agents/[agentID]/page.tsx
@@ -11,20 +11,13 @@ import CoverPhoto03 from "@/assets/images/cover/cover-03.jpg";
 import CoverPhoto04 from "@/assets/images/cover/cover-04.jpg";
 import CoverPhoto05 from "@/assets/images/cover/cover-05.jpg";
 
-// interface AgentProfilePageProps {
-//   params: {
-//     agentID: string;
-//   };
-//   [key: string]: unknown; // allow other injected props
-// }
-
-export default async function AgentProfilePage({
-  params,
-}: {
+type PageProps = {
   params: {
     agentID: string;
   };
-}) {
+};
+
+export default async function AgentProfilePage({ params }: PageProps) {
   const agentIDNumber = Number(params.agentID); // convert to number
   const num: number = Math.floor(Math.random() * 5) + 1;
   let coverPhoto;
